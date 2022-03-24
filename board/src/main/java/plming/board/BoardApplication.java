205a2969efb8f3680f2c1f72ccdc189067ab3466
SulimChoi <66582313+SulimChoi@users.noreply.github.com>
2022-03-25 08:21:58 +0900

    Build: MySQL 연동 코드 추가
    Spring 서버와 MySQL 연동
    	- build.gradle, application.properties: MySQL 연동과 관련된 코드 추가
    	- DBConfiguration.java: MySQL 연동 설정 파일
    	- BoardApplicationTests.java: MySQL 연동 테스트 코드 추가

--- board/src/main/java/plming/board/BoardApplication.java
+++ board/src/main/java/plming/board/BoardApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BoardApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BoardApplication.class, args);
 	}
 
