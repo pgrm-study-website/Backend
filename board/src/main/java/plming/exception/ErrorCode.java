d0c320e918aa668b39f45895aa789e4a5db681ed
JMsuper <tjsqls2067@naver.com>
2022-04-11 20:40:36 +0900

    Feat: 사용자 조회(닉네임) 및 사용자 조회(ID) 추가 ErrorCode.java : 사용자 조회 실패 Error Code 추가

--- board/src/main/java/plming/exception/ErrorCode.java
+++ board/src/main/java/plming/exception/ErrorCode.java
@@ -46,7 +46,7 @@ public enum ErrorCode {
     /*
      * 401 UNAUTHORIZED: 로그인 실패
      */
-    LOGIN_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "계정정보가 잘못되었습니다."),
+    LOGIN_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "계정정보가 틀렸습니다."),
 
     /*
      * 403 FORBIDDEN: 권한이 없음
@@ -62,6 +62,11 @@ public enum ErrorCode {
      * 400 BAD_REQUEST: 잘못된 요청
      */
     BAD_SEARCH(HttpStatus.BAD_REQUEST, "잘못된 검색 입니다."),
+
+    /*
+     * 404 NOT_FOUND: 사용자 조회 실패
+     */
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND,"사용자 정보를 찾을 수 없습니다."),
 
     /*
      * 404 NOT_FOUND: 리소스를 찾을 수 없음
