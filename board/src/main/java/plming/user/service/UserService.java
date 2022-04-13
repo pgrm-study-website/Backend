79c7128f677ad8051111f09379939cdff8f93828
JMsuper <tjsqls2067@naver.com>
2022-04-13 21:08:43 +0900

    Fix: ErrorCode에 충돌난 부분 수정

--- board/src/main/java/plming/user/service/UserService.java
+++ board/src/main/java/plming/user/service/UserService.java
@@ -40,16 +40,21 @@ public class UserService{
     public UserResponseDto getUserByNickName(String nickName) {
         User user = userRepository.findByNickname(nickName)
 <<<<<<< HEAD
+<<<<<<< HEAD
                 .orElseThrow(()-> new CustomException(ErrorCode.USERS_NOT_FOUND));
 =======
           .orElseThrow(()-> new CustomException(ErrorCode.USERS_NOT_FOUND));
 >>>>>>> 5fbd76f... Fix: ErrorCode에 충돌난 부분 수정
+=======
+                .orElseThrow(()-> new CustomException(ErrorCode.USERS_NOT_FOUND));
+>>>>>>> 67d88c8... Fix: ErrorCode에 충돌난 부분 수정
         return new UserResponseDto(user);
     }
 
     public UserResponseDto getUserByUserId(Long userId) {
         User user = userRepository.findById(userId)
                 .orElseThrow(()-> new CustomException(ErrorCode.USERS_NOT_FOUND));
+<<<<<<< HEAD
 <<<<<<< HEAD
         return new UserResponseDto(user);
     }
@@ -59,6 +64,8 @@ public class UserService{
                 .orElseThrow(()-> new CustomException(ErrorCode.USERS_NOT_FOUND));
 =======
 >>>>>>> 5fbd76f... Fix: ErrorCode에 충돌난 부분 수정
+=======
+>>>>>>> 67d88c8... Fix: ErrorCode에 충돌난 부분 수정
         return new UserResponseDto(user);
     }
 
@@ -90,18 +97,25 @@ public class UserService{
 =======
     public void deleteUser(HttpServletRequest request,Long userId) {
 <<<<<<< HEAD
+<<<<<<< HEAD
         User user = userRepository.findById(userId).orElseThrow(()-> new CustomException(ErrorCode.USER_NOT_FOUND));
 >>>>>>> ce79537... Fix: 닉네임 중복 오류 및 사용자 삭제 deleteYn 추가
 =======
+=======
+>>>>>>> 67d88c8... Fix: ErrorCode에 충돌난 부분 수정
         User user = userRepository.findById(userId).orElseThrow(()-> new CustomException(ErrorCode.USERS_NOT_FOUND));
         if(user.getDeleteYn() == '1'){
             throw new CustomException(ErrorCode.ALREADY_DELETE);
         }
+<<<<<<< HEAD
 >>>>>>> 5fbd76f... Fix: ErrorCode에 충돌난 부분 수정
+=======
+>>>>>>> 67d88c8... Fix: ErrorCode에 충돌난 부분 수정
         user.delete();
     }
 
     public void checkPassword(Long userId, String password) {
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
         User user = userRepository.findById(userId).orElseThrow(()->new CustomException(ErrorCode.USERS_NOT_FOUND));
@@ -111,6 +125,9 @@ public class UserService{
 =======
         User user = userRepository.findById(userId).orElseThrow(()->new CustomException(ErrorCode.USERS_NOT_FOUND));
 >>>>>>> 5fbd76f... Fix: ErrorCode에 충돌난 부분 수정
+=======
+        User user = userRepository.findById(userId).orElseThrow(()->new CustomException(ErrorCode.USERS_NOT_FOUND));
+>>>>>>> 67d88c8... Fix: ErrorCode에 충돌난 부분 수정
         if(!bCryptPasswordEncoder.matches(password,user.getPassword())){
             throw new CustomException(ErrorCode.BAD_REQUEST);
         }
@@ -121,6 +138,7 @@ public class UserService{
         User updateUser = userRepository.findById(userId)
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
                 .orElseThrow(()->new CustomException(ErrorCode.USERS_NOT_FOUND));
 =======
                 .orElseThrow(()->new CustomException(ErrorCode.USER_NOT_FOUND));
@@ -128,6 +146,9 @@ public class UserService{
 =======
                 .orElseThrow(()->new CustomException(ErrorCode.USERS_NOT_FOUND));
 >>>>>>> 5fbd76f... Fix: ErrorCode에 충돌난 부분 수정
+=======
+                .orElseThrow(()->new CustomException(ErrorCode.USERS_NOT_FOUND));
+>>>>>>> 67d88c8... Fix: ErrorCode에 충돌난 부분 수정
         updateUser.updatePassword(bCryptPasswordEncoder.encode(password));
     }
 
