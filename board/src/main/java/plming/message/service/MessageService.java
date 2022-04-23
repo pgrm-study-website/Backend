75f3ff18dd047cf0f3caa075b57c6e1f032ac888
JMsuper <80041449+JMsuper@users.noreply.github.com>
2022-04-23 18:53:11 +0900

    Fix: user 삭제 api 수정 (#44)

--- board/src/main/java/plming/message/service/MessageService.java
+++ board/src/main/java/plming/message/service/MessageService.java
@@ -13,7 +13,7 @@ import plming.message.entity.Message;
 import plming.message.entity.MessageRepository;
 import plming.notification.entity.NotificationType;
 import plming.user.entity.User;
-import plming.user.service.UserService;
+import plming.user.entity.UserRepository;
 
 import javax.transaction.Transactional;
 import java.util.List;
@@ -26,38 +26,44 @@ public class MessageService {
     private MessageRepository messageRepository;
 
     @Autowired
-    private UserService userService;
+    private UserRepository userRepository;
 
     @Autowired
     private ApplicationEventPublisher eventPublisher;
 
 
     public List<MessageGroupResponseDto> getMessageGroupList(Long userId){
-        User user = userService.getUserById(userId);
+        User user = getUserById(userId);
         List<Message> messageList = messageRepository.findMessageGroupByUser(user);
         return messageList.stream().map(m -> new MessageGroupResponseDto(m, userId)).collect(Collectors.toList());
     }
 
     public List<MessageDetailResponseDto> getMessageDetailList(Long userId, Long otherId){
-        User user = userService.getUserById(userId);
-        User other = userService.getUserById(otherId);
+        User user = getUserById(userId);
+        User other = getUserById(otherId);
         List<Message> messageList = messageRepository.findMessageByUserAndOther(user,other);
         return messageList.stream().map(m -> new MessageDetailResponseDto(m,userId)).collect(Collectors.toList());
     }
 
     @Transactional
     public MessageDetailResponseDto postMessage(MessageRequestDto messageRequestDto){
-        User user = userService.getUserById(messageRequestDto.getUserId());
-        User other = userService.getUserById(messageRequestDto.getOtherId());
+        User user = getUserById(messageRequestDto.getUserId());
+        User other = getUserById(messageRequestDto.getOtherId());
         Message message = messageRepository.save(messageRequestDto.toEntity(user,other));
         eventPublisher.publishEvent(new MessageCreateEvent(message, message.getReceiver(), NotificationType.message));
         return new MessageDetailResponseDto(message,messageRequestDto.getUserId());
     }
 
+    public void deleteAllMessageByUserId(Long userId){
+        User user = getUserById(userId);
+        List<Message> messageList = messageRepository.findAllBySenderOrReceiver(user,user);
+        messageList.forEach(m -> deleteMessage(userId, m.getId()));
+    }
+
     @Transactional
     public void deleteAllMessage(Long userId, Long otherId){
-        User user = userService.getUserById(userId);
-        User other = userService.getUserById(otherId);
+        User user = getUserById(userId);
+        User other = getUserById(otherId);
 
         List<Message> messageList = messageRepository.findMessageByUserAndOther(user,other);
         for(Message m : messageList){
@@ -72,7 +78,7 @@ public class MessageService {
 
     @Transactional
     public void deleteMessage(Long userId, Long messageId){
-        User user = userService.getUserById(userId);
+        User user = getUserById(userId);
         Message message = messageRepository.findById(messageId).orElseThrow(()-> new CustomException(ErrorCode.MESSAGE_NOT_FOUND));
 
         if(message.getSender().getId().equals(user.getId())){
@@ -85,5 +91,8 @@ public class MessageService {
         if(message.hasToBeDeleted()) messageRepository.delete(message);
     }
 
+    private User getUserById(Long userId){
+        return userRepository.findById(userId).orElseThrow(()-> new CustomException(ErrorCode.USERS_NOT_FOUND));
+    }
 
 }
