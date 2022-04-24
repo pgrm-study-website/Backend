143fdf07b30eae0813c65b4897a6fccf6df71544
Slchoi <66582313+SulimChoi@users.noreply.github.com>
2022-04-24 11:53:41 +0900

    Fix: Notifiaction 객체 수정 (#46)
    * Fix: Notifiaction 객체 수정
    
    * Fix: 불필요한 코드 제거

--- board/src/main/java/plming/notification/dto/NotificationDto.java
+++ board/src/main/java/plming/notification/dto/NotificationDto.java
@@ -4,6 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import plming.notification.entity.Notification;
+import plming.notification.entity.NotificationType;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -13,9 +17,11 @@ public class NotificationDto {
     private Long id;
     private String content;
     private String url;
+    private NotificationType type;
+    private LocalDateTime createDate;
 
     public static NotificationDto create(Notification notification) {
         return new NotificationDto(notification.getId(), notification.getContent(),
-                notification.getUrl());
+                notification.getUrl(), notification.getNotificationType(), notification.getCreateDate());
     }
 }
