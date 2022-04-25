37ad2d50cd46aa9d13cc4e32b790d29147ff753b
SulimChoi <66582313+SulimChoi@users.noreply.github.com>
2022-04-25 19:37:04 +0900

    Fix: message event url 값 변경

--- board/src/main/java/plming/notification/entity/NotificationType.java
+++ board/src/main/java/plming/notification/entity/NotificationType.java
@@ -11,7 +11,7 @@ public enum NotificationType {
     reject("게시글에 참여가 거절되었습니다.", "/posts/"),
     comment("에 댓글이 달렸습니다.", "/posts/"),
     recomment("에 대댓글이 달렸습니다.", "/posts/"),
-    message(" 회원에게 메세지가 도착했습니다.", "/message");
+    message(" 회원에게 메세지가 도착했습니다.", "/message/");
 
     private String content;
     private String url;
@@ -24,11 +24,7 @@ public enum NotificationType {
     public String makeContent(String title) {
         return "'" + title + "'" + content;
     }
-
     public String makeUrl(Long id) {
         return url + id;
-    }
-    public String getUrl() {
-        return url;
     }
 }
